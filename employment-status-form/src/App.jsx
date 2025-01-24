cf6491deed8b5def164be53461de544e99ecab07
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-24 11:48:20 +0100

    radio buttons

--- employment-status-form/src/App.jsx
+++ employment-status-form/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
   function signUp(formData) {
-    const email = formData.get("email")
-    const password = formData.get("password")
-      console.log(email,password)
-    }
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const employmentStatus = formData.get("employmentStatus");
+    console.log(email, password,employmentStatus);
+  }
 
-  return(
+  return (
     <section>
       <h1>Signup form</h1>
       <form action={signUp}>
-
         <label htmlFor="email">Email:</label>
-        <input id="email" defaultValue="dona@paulsen.com" type="email" name="email" placeholder="dona@paulsen.com" />
+        <input
+          id="email"
+          defaultValue="dona@paulsen.com"
+          type="email"
+          name="email"
+          placeholder="dona@paulsen.com"
+        />
 
         <label htmlFor="password">Password:</label>
-        <input id="password" defaultValue="password123" type="password" name="password" />
+        <input
+          id="password"
+          defaultValue="password123"
+          type="password"
+          name="password"
+        />
 
         <label htmlFor="description">Description:</label>
-        <textarea id="description" name="description"></textarea>
-        
-        
+        <textarea
+          id="description"
+          name="description"
+          defaultValue="This is a description"
+        ></textarea>
 
-        
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time" />
+            Full-time
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="internship" />
+            Internship
+          </label>
+        </fieldset>
+
         <button>Submit</button>
-
       </form>
     </section>
-  )
+  );
 }
-
